a92493e0d70bb1b3ce6e520d3d9ab995b7131dd1
Marc Weber <maweber@mpi-bremen.de>
2015-07-06 21:39:58 +0200

    develop clean css

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
-/*
-$(document).ready(function () {
-    $('.your-class').slick({
-        setting_name : 'xxx'
-    });
-});*/
 $(document).foundation('section').foundation('orbit', {
     animation: 'fade',
     timer_speed: 3000,
     animation_speed: 400,
     stack_on_small: false,
-    navigation_arrows: false,
+    navigation_arrows: true,
     slide_number: false,
     pause_on_hover: false,
     resume_on_mouseout: false,
@@ -17,3 +11,7 @@ $(document).foundation('section').foundation('orbit', {
     timer: true,
     variable_height: false,
 });
+
+$('#slide-1').backstretch("img/marc_blue.jpg");
+$('#slide-2').backstretch("img/marc_blue.jpg");
+$('#slide-3').backstretch("img/marc_blue.jpg");
