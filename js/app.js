85d6645b76e4a2b760274d5cc221e6b392fcfd6d
Marc Weber <maweber@mpi-bremen.de>
2016-02-17 23:29:39 +0100

    develop add basic listener for sticky events

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+console.log(Foundation.version);
+
+$('.title-bar').on('sticky.zf.stuckto:top', function(){
+    console.log('stuckto');
+    $(this).css('background-color', 'red');
+    // TODO: switch to addClass / removeClass, add logo on move
+
+}).on('sticky.zf.unstuckfrom:top', function(){
+    console.log('unstuckto');
+    $(this).css('background-color', 'lime');
+})
