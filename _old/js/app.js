f0aafbbb2666e1ca57eefe27471f0e2215829263
Marc Weber <maweber@mpi-bremen.de>
2016-02-17 21:44:08 +0100

    develop move previous foundation 5 stuff to _old and add fresh foundation6 for sites complete package

---
+++ _old/js/app.js
@@ -0,0 +1,17 @@
+$(document).foundation('section').foundation('orbit', {
+    animation: 'fade',
+    timer_speed: 3000,
+    animation_speed: 400,
+    stack_on_small: false,
+    navigation_arrows: true,
+    slide_number: false,
+    pause_on_hover: false,
+    resume_on_mouseout: false,
+    bullets: false,
+    timer: true,
+    variable_height: false,
+});
+
+$('#slide-1').backstretch("img/marc_blue.jpg");
+$('#slide-2').backstretch("img/marc_blue.jpg");
+$('#slide-3').backstretch("img/marc_blue.jpg");
