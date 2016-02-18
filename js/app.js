666425b0518c66b4599dac6a5cd70c71b21c4811
Marc Weber <maweber@mpi-bremen.de>
2016-02-18 22:48:36 +0100

    develop some work on top navi, add logo

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ console.log(Foundation.version);
 
 $('.title-bar').on('sticky.zf.stuckto:top', function(){
     console.log('stuckto');
-    $(this).css('background-color', 'red');
+    // $(this).css('background-color', 'red');
     // TODO: switch to addClass / removeClass, add logo on move
 
 }).on('sticky.zf.unstuckfrom:top', function(){
     console.log('unstuckto');
-    $(this).css('background-color', 'lime');
+    // $(this).css('background-color', 'lime');
 })
