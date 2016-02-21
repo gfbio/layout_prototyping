acf2df2e17e6580f4f50e50d54fb1fba658544f8
Marc Weber <maweber@mpi-bremen.de>
2016-02-21 21:17:00 +0100

    develop style sticky navi
    - add images

--- js/app.js
+++ js/app.js
@@ -2,21 +2,16 @@ $(document).foundation();
 console.log(Foundation.version);
 
 $('.title-bar').on('sticky.zf.stuckto:top', function(){
-    console.log('stuckto');
     $(this).addClass('nav-move');
-    $('li#brand').removeClass('logo-top-nav');
     $('li#brand').addClass('brand-move');
-    $('li#brand-placeholder').removeClass('logo-top-nav-no-img');
     $('li#brand-placeholder').addClass('brand-placeholder-move');
-    // $(this).css('background-color', 'red');
-    // TODO: switch to addClass / removeClass, add logo on move
+    $('li#brand').removeClass('logo-top-nav');
+    $('li#brand-placeholder').removeClass('logo-top-nav-no-img');
 
 }).on('sticky.zf.unstuckfrom:top', function(){
-    console.log('unstuckto');
     $(this).removeClass('nav-move');
     $('li#brand').removeClass('brand-move');
-    $('li#brand').addClass('logo-top-nav');
     $('li#brand-placeholder').removeClass('brand-placeholder-move');
+    $('li#brand').addClass('logo-top-nav');
     $('li#brand-placeholder').addClass('logo-top-nav-no-img');
-    // $(this).css('background-color', 'lime');
 })
