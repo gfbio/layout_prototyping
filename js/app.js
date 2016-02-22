2ae9ae60c6243d437a4be65d8cd482e1244c1e49
Marc Weber <maweber@mpi-bremen.de>
2016-02-22 22:29:14 +0100

    develop clean comments from  work on second navi
    - add mega menu example

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).foundation();
-console.log(Foundation.version);
+//console.log(Foundation.version);
 
-$('.title-bar').on('sticky.zf.stuckto:top', function(){
+$('.title-bar').on('sticky.zf.stuckto:top', function () {
     $(this).addClass('nav-move');
     $('li#brand').addClass('brand-move');
     $('li#brand-placeholder').addClass('brand-placeholder-move');
     $('li#brand').removeClass('logo-top-nav');
     $('li#brand-placeholder').removeClass('logo-top-nav-no-img');
 
-}).on('sticky.zf.unstuckfrom:top', function(){
+}).on('sticky.zf.unstuckfrom:top', function () {
     $(this).removeClass('nav-move');
     $('li#brand').removeClass('brand-move');
     $('li#brand-placeholder').removeClass('brand-placeholder-move');
