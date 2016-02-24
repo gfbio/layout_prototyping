5aaad3aa7b5e4fdbc94cf63cd5c394da18509cf5
Marc Weber <maweber@mpi-bremen.de>
2016-02-24 21:48:02 +0100

    develop clean add mega menu style solution for f6

--- js/app.js
+++ js/app.js
@@ -14,4 +14,13 @@ $('.title-bar').on('sticky.zf.stuckto:top', function () {
     $('li#brand-placeholder').removeClass('brand-placeholder-move');
     $('li#brand').addClass('logo-top-nav');
     $('li#brand-placeholder').addClass('logo-top-nav-no-img');
-})
+});
+
+$('.is-hover a').on('mouseover', function () {
+    var idx = $(this).parent().index() + 2;
+    $('.info:nth-child(' + idx + ')').addClass('info-visible');
+    $('.info:nth-child(' + idx + ')').siblings().removeClass('info-visible');
+});
+$('.dropdown').on('mouseout', function () {
+    $('.info:nth-child(1)').addClass('info-visible').siblings().removeClass('info-visible');
+});
