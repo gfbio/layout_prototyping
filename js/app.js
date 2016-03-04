82cd116e28504b348fa80396802fab838d71ecee
Marc Weber <maweber@mpi-bremen.de>
2016-03-04 21:45:51 +0100

    develop logo is now link
    - adapt js for sticky navi

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ $(document).foundation();
 
 $('.title-bar').on('sticky.zf.stuckto:top', function () {
     $(this).addClass('nav-move');
-    $('li#brand').addClass('brand-move');
-    $('li#brand-placeholder').addClass('brand-placeholder-move');
-    $('li#brand').removeClass('logo-top-nav');
-    $('li#brand-placeholder').removeClass('logo-top-nav-no-img');
+    $('#brand').addClass('brand-move');
+    $('#brand-placeholder').addClass('brand-placeholder-move');
+    $('#brand').removeClass('logo-top-nav');
+    $('#brand-placeholder').removeClass('logo-top-nav-no-img');
 
 }).on('sticky.zf.unstuckfrom:top', function () {
     $(this).removeClass('nav-move');
-    $('li#brand').removeClass('brand-move');
-    $('li#brand-placeholder').removeClass('brand-placeholder-move');
-    $('li#brand').addClass('logo-top-nav');
-    $('li#brand-placeholder').addClass('logo-top-nav-no-img');
+    $('#brand').removeClass('brand-move');
+    $('#brand-placeholder').removeClass('brand-placeholder-move');
+    $('#brand').addClass('logo-top-nav');
+    $('#brand-placeholder').addClass('logo-top-nav-no-img');
 });
 
 $('.is-hover a').on('mouseover', function () {
