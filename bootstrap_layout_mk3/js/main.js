ee9741dc3922102fdf9f0c0379a98c9751ced960
Marc Weber <maweber@mpi-bremen.de>
2016-05-18 21:25:33 +0200

    GFBIO-1328 mega-menu hover

--- bootstrap_layout_mk3/js/main.js
+++ bootstrap_layout_mk3/js/main.js
@@ -1,77 +1,77 @@
 /* ========================================================================= */
 /*	Preloader
-/* ========================================================================= */
+ /* ========================================================================= */
 
-jQuery(window).load(function(){
+jQuery(window).load(function () {
 
-	$("#preloader").fadeOut("slow");
+    $("#preloader").fadeOut("slow");
 
 });
 
 /* ========================================================================= */
 /*  Welcome Section Slider
-/* ========================================================================= */
+ /* ========================================================================= */
 
-$(function() {
+$(function () {
 
-    var Page = (function() {
+    var Page = (function () {
 
-        var $navArrows = $( '#nav-arrows' ),
-            $nav = $( '#nav-dots > span' ),
-            slitslider = $( '#slider' ).slitslider( {
-                onBeforeChange : function( slide, pos ) {
+        var $navArrows = $('#nav-arrows'),
+            $nav = $('#nav-dots > span'),
+            slitslider = $('#slider').slitslider({
+                onBeforeChange: function (slide, pos) {
 
-                    $nav.removeClass( 'nav-dot-current' );
-                    $nav.eq( pos ).addClass( 'nav-dot-current' );
+                    $nav.removeClass('nav-dot-current');
+                    $nav.eq(pos).addClass('nav-dot-current');
 
                 }
-            } ),
+            }),
 
-            init = function() {
+            init = function () {
 
                 initEvents();
 
             },
-            initEvents = function() {
+            initEvents = function () {
 
                 // add navigation events
-                $navArrows.children( ':last' ).on( 'click', function() {
+                $navArrows.children(':last').on('click', function () {
 
                     slitslider.next();
                     return false;
 
-                } );
+                });
 
-                $navArrows.children( ':first' ).on( 'click', function() {
+                $navArrows.children(':first').on('click', function () {
 
                     slitslider.previous();
                     return false;
 
-                } );
+                });
 
-                $nav.each( function( i ) {
+                $nav.each(function (i) {
 
-                    $( this ).on( 'click', function( event ) {
+                    $(this).on('click', function (event) {
 
-                        var $dot = $( this );
+                        var $dot = $(this);
 
-                        if( !slitslider.isActive() ) {
+                        if (!slitslider.isActive()) {
 
-                            $nav.removeClass( 'nav-dot-current' );
-                            $dot.addClass( 'nav-dot-current' );
+                            $nav.removeClass('nav-dot-current');
+                            $dot.addClass('nav-dot-current');
 
                         }
 
-                        slitslider.jump( i + 1 );
+                        slitslider.jump(i + 1);
                         return false;
 
-                    } );
+                    });
 
-                } );
+                });
 
             };
 
-            return { init : init };
+        return {init: init};
 
     })();
 
@@ -80,28 +80,28 @@ $(function() {
 });
 
 
+$(document).ready(function () {
 
-$(document).ready(function(){
+    /* ========================================================================= */
+    /*	Menu item highlighting
+     /* ========================================================================= */
 
-	/* ========================================================================= */
-	/*	Menu item highlighting
-	/* ========================================================================= */
+    jQuery('#nav-left').singlePageNav({
+        offset: jQuery('#nav-left').outerHeight(),
+        filter: ':not(.external)',
+        speed: 2000,
+        currentClass: 'current',
+        easing: 'easeInOutExpo',
+        updateHash: true,
+        beforeStart: function () {
+            console.log('begin scrolling');
+        },
+        onComplete: function () {
+            console.log('done scrolling');
+        }
+    });
 
-	jQuery('#nav-left').singlePageNav({
-		offset: jQuery('#nav-left').outerHeight(),
-		filter: ':not(.external)',
-		speed: 2000,
-		currentClass: 'current',
-		easing: 'easeInOutExpo',
-		updateHash: true,
-		beforeStart: function() {
-			console.log('begin scrolling');
-		},
-		onComplete: function() {
-			console.log('done scrolling');
-		}
-	});
-
+    // TODO: no need for singlepage nav at all ?
     // jQuery('#nav-right').singlePageNav({
     //     offset: jQuery('#nav-right').outerHeight(),
     //     filter: ':not(.external)',
@@ -118,21 +118,19 @@ $(document).ready(function(){
     // });
 
     // yamm megamenu
-    $(document).on('click', '.yamm .dropdown-menu', function(e) {
+    $(document).on('click', '.yamm .dropdown-menu', function (e) {
         e.stopPropagation()
     });
 
-    // $(".dropdown").hover(
-    //     function() {
-    //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-    //         $(this).toggleClass('open');
-    //     },
-    //     function() {
-    //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-    //         $(this).toggleClass('open');
-    //     }
-    // );
 
+    // Dropdown Menu Fade
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).stop().fadeIn("slow");
+        },
+        function () {
+            $('.dropdown-menu', this).stop().fadeOut("slow");
+        });
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 100) {
@@ -152,65 +150,66 @@ $(document).ready(function(){
         // }
     });
 
-	/* ========================================================================= */
-	/*	Fix Slider Height
-	/* ========================================================================= */
+    /* ========================================================================= */
+    /*	Fix Slider Height
+     /* ========================================================================= */
 
 
     // Slider Height
     // FIXME: why is there 15px gap on bottom of section ?
-    var slideHeight = $(window).height()+15;
-    console.log('slideHeight '+slideHeight);
+    var slideHeight = $(window).height() + 15;
+    console.log('slideHeight ' + slideHeight);
 
-    $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
+    $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
 
-    $(window).resize(function(){'use strict',
-        $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
+    $(window).resize(function () {
+        'use strict',
+            $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
     });
 
 
     /* TODO: put this back in if slider */
     $("#works, #testimonial").owlCarousel({
-		navigation : true,
-		pagination : false,
-		slideSpeed : 700,
-		paginationSpeed : 400,
-		singleItem:true,
-		navigationText: ["<i class='fa fa-angle-left fa-lg'></i>","<i class='fa fa-angle-right fa-lg'></i>"]
+        navigation: true,
+        pagination: false,
+        slideSpeed: 700,
+        paginationSpeed: 400,
+        singleItem: true,
+        navigationText: ["<i class='fa fa-angle-left fa-lg'></i>", "<i class='fa fa-angle-right fa-lg'></i>"]
     });
 
 
-	/* ========================================================================= */
-	/*	Featured Project Lightbox
-	/* ========================================================================= */
+    /* ========================================================================= */
+    /*	Featured Project Lightbox
+     /* ========================================================================= */
 
-	$(".fancybox").fancybox({
-		padding: 0,
+    $(".fancybox").fancybox({
+        padding: 0,
 
-		openEffect : 'elastic',
-		openSpeed  : 650,
+        openEffect: 'elastic',
+        openSpeed: 650,
 
-		closeEffect : 'elastic',
-		closeSpeed  : 550,
+        closeEffect: 'elastic',
+        closeSpeed: 550,
 
-		closeClick : true,
+        closeClick: true,
 
-		beforeShow: function () {
-			this.title = $(this.element).attr('title');
-			this.title = '<h3>' + this.title + '</h3>' + '<p>' + $(this.element).parents('.portfolio-item').find('img').attr('alt') + '</p>';
-		},
+        beforeShow: function () {
+            this.title = $(this.element).attr('title');
+            this.title = '<h3>' + this.title + '</h3>' + '<p>' + $(this.element).parents('.portfolio-item').find('img').attr('alt') + '</p>';
+        },
 
-		helpers : {
-			title : {
-				type: 'inside'
-			},
-			overlay : {
-				css : {
-					'background' : 'rgba(0,0,0,0.8)'
-				}
-			}
-		}
-	});
+        helpers: {
+            title: {
+                type: 'inside'
+            },
+            overlay: {
+                css: {
+                    'background': 'rgba(0,0,0,0.8)'
+                }
+            }
+        }
+    });
 
 });
 
@@ -224,25 +223,25 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 
-	    var myLatLng = new google.maps.LatLng(53.1066684, 8.8518396);
+    var myLatLng = new google.maps.LatLng(53.1066684, 8.8518396);
 
-	    var mapOptions = {
-	        zoom: 15,
-	        center: myLatLng,
-	        disableDefaultUI: true,
-	        scrollwheel: false,
-	        navigationControl: true,
-	        mapTypeControl: false,
-	        scaleControl: false,
-	        draggable: true,
+    var mapOptions = {
+        zoom: 15,
+        center: myLatLng,
+        disableDefaultUI: true,
+        scrollwheel: false,
+        navigationControl: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: true,
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
-            styles: [{"stylers": [{"hue": "#345AA2"}, {"saturation": 250}]}, {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [{"lightness": 50}, {"visibility": "simplified"}]
-            }, {"featureType": "road", "elementType": "labels", "stylers": [{"visibility": "off"}]}]
+        styles: [{"stylers": [{"hue": "#345AA2"}, {"saturation": 250}]}, {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{"lightness": 50}, {"visibility": "simplified"}]
+        }, {"featureType": "road", "elementType": "labels", "stylers": [{"visibility": "off"}]}]
     };
 
     // Get the HTML DOM element that will contain your map 
@@ -256,15 +255,15 @@ function init() {
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(22.402789, 91.822156),
         map: map,
-		icon: 'img/icons/map-marker.png',
+        icon: 'img/icons/map-marker.png',
     });
 }
 
 // ========== END GOOGLE MAP ========== //
 
-var wow = new WOW ({
-	offset:       75,          // distance to the element when triggering the animation (default is 0)
-	mobile:       false,       // trigger animations on mobile devices (default is true)
+var wow = new WOW({
+    offset: 75,          // distance to the element when triggering the animation (default is 0)
+    mobile: false,       // trigger animations on mobile devices (default is true)
 });
 wow.init();
 
